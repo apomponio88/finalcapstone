5f3b99641e76be047741fbab555c7f8b9d57cc5a
Andrew Pomponio <apomponio@gmail.com>
2022-12-14 08:39:30 -0500

    I AM THE SENATE

--- vue/src/components/SiteIntro.vue
+++ vue/src/components/SiteIntro.vue
@@ -1,7 +1,7 @@
 <template>
   <div id='head'>
     <img class="header" src="https://i0.wp.com/www.jamiesale-cartoonist.com/wp-content/uploads/cartoon-robot-free.png?ssl=1" alt="">
-    <h1>Welcome To TechBot!</h1>
+    <h1 class="welcome-header">Welcome To TechBot!</h1>
   </div>
 </template>
 
